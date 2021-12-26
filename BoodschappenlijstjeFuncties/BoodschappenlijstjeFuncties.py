f12ba55348792df64e8b6913911a7bde6098fc9c
99063871 <99063871@mydavinci.com>
2021-12-26 13:48:07 +0100

    random variable a verwijderd en feature toegevoegd: spaties worden verwijderd

--- BoodschappenlijstjeFuncties/BoodschappenlijstjeFuncties.py
+++ BoodschappenlijstjeFuncties/BoodschappenlijstjeFuncties.py
@@ -2,12 +2,10 @@ boodschappenlijst = {
 
 }
 
-a = 1
-
 def boodschappenlijstFunc():
-    item = input("Wat wilt u aan uw boodschappenlijstje toevoegen?")
+    item = input("Wat wilt u aan uw boodschappenlijstje toevoegen?").strip()
     if item != "stop":
-        amount = input("Hoeveel "+item+" wilt u toevoegen?")
+        amount = input("Hoeveel "+item+" wilt u toevoegen?").strip()
         boodschappenlijst[item] = int(amount)
         boodschappenlijstFunc()
     else:
