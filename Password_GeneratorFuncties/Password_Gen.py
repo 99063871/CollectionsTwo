74e8d676b02ba20721e465af5e238572e37f8872
99063871 <99063871@mydavinci.com>
2022-01-11 09:54:26 +0100

    er worden 8 tot 20 random kleine letters gekozen en in de list gezet

--- Password_GeneratorFuncties/Password_Gen.py
+++ Password_GeneratorFuncties/Password_Gen.py
@@ -6,4 +6,7 @@ password = []
 amountLetter = random.randint(2,6)
 for a in range(amountLetter):
     password.append(random.choice(string.ascii_letters).upper())
+
+for x in range(8, 20):
+    password.append(random.choice(string.ascii_letters).lower())
 print(password)
