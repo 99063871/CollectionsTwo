0a71c12b8513e139f298a02cc85ddf8d7efe063c
99063871 <99063871@mydavinci.com>
2022-01-11 10:31:26 +0100

    alles word aangemaakt en in een list gezet, de list word geshuffeld op het einde

--- Password_GeneratorFuncties/Password_Gen.py
+++ Password_GeneratorFuncties/Password_Gen.py
@@ -3,10 +3,23 @@ import string
 
 password = []
 
+specialReeks = ["@", "#", "$", "%", "&", "_", "?"]
+
 amountLetter = random.randint(2,6)
+amountDigit = random.randint(4,7)
+
 for a in range(amountLetter):
     password.append(random.choice(string.ascii_letters).upper())
 
 for x in range(8, 20):
     password.append(random.choice(string.ascii_letters).lower())
+
+for c in range(3):
+    password.append(random.choice(specialReeks))
+
+for c in range(amountDigit):
+    password.append(random.choice(string.digits))
+
+print(password)
+random.shuffle(password)
 print(password)
