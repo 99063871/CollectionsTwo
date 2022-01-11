e10faf628df9afedf2d58a6d896363e4a5698d2a
99063871 <99063871@mydavinci.com>
2022-01-11 09:37:41 +0100

    random kleine letter was ik vergeten, staat er nu in

--- Password_GeneratorFuncties/Password_Gen.py
+++ Password_GeneratorFuncties/Password_Gen.py
@@ -5,9 +5,11 @@ string.ascii_letters
 string.digits
 string.punctuation
 
-randomletter = random.choice(string.ascii_letters).upper()
+randomletter = random.choice(string.ascii_letters)
+randomletterC = random.choice(string.ascii_letters).upper()
 randomdigit = random.choice(string.digits)
 randomspecial = random.choice(string.punctuation)
 print(randomletter)
+print(randomletterC)
 print(randomdigit)
 print(randomspecial)
