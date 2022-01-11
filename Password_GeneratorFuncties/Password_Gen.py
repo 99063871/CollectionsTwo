38271d2d17584e455177c78e296af5040da850c7
99063871 <99063871@mydavinci.com>
2022-01-11 09:18:22 +0100

    alfabet in een list gezet, 1 zelf en 1 automatisch

---
+++ Password_GeneratorFuncties/Password_Gen.py
@@ -0,0 +1,5 @@
+alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alfabetH = []
+
+for a in range(len(alfabet)):
+    alfabetH.append(alfabet[a].upper())
