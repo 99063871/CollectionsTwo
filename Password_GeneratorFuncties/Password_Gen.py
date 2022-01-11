2ebe40661d6f33048b0eb13126803cd072d985be
99063871 <99063871@mydavinci.com>
2022-01-11 11:04:16 +0100

    password generator gemaakt, alles word gemaakt en gechekt

--- Password_GeneratorFuncties/Password_Gen.py
+++ Password_GeneratorFuncties/Password_Gen.py
@@ -5,21 +5,33 @@ password = []
 
 specialReeks = ["@", "#", "$", "%", "&", "_", "?"]
 
-amountLetter = random.randint(2,6)
-amountDigit = random.randint(4,7)
+amountLetter = random.randint(2,6)#kiest een random getal tussen 2 en 6
+amountDigit = random.randint(4,7)#kiest een random getal tussen 4 en 7
 
-for a in range(amountLetter):
+for a in range(amountLetter):#deze loop pakt een random hoofdletter
     password.append(random.choice(string.ascii_letters).upper())
 
-for x in range(8, 20):
-    password.append(random.choice(string.ascii_letters).lower())
-
-for c in range(3):
+for c in range(3):#deze loop pakt 3 random tekens uit de specialReeks
     password.append(random.choice(specialReeks))
 
-for c in range(amountDigit):
+for c in range(amountDigit):#deze loop pakt random getallen
     password.append(random.choice(string.digits))
 
-print(password)
-random.shuffle(password)
-print(password)
+amountLetterL = 24 - len(password)#dit berekend hoeveel er nog bij moet om op 24 te komen
+for x in range(amountLetterL):#deze loop pakt random kleine letters
+    password.append(random.choice(string.ascii_letters).lower())
+random.shuffle(password)#dit schud de list
+
+def checkFunc():#deze function checkt of op 1,2,3 of de laatste een str zijn, als dat niet het geval is word er opnieuw geschud en gecheckt, als het klopt word het geprint
+    strcheck1 = password[0].isalpha()
+    strcheck2 = password[1].isalpha()
+    strcheck3 = password[2].isalpha()
+    strcheckLast = password[23].isalpha()
+
+    if strcheck1 == False or strcheck2 == False or strcheck3 == False or strcheckLast == False:
+        random.shuffle(password)
+        checkFunc()
+    else:
+        print(password)
+
+checkFunc()
