4eaa82730e9c3d1613ae47781484312b2f4421a1
99063871 <99063871@mydavinci.com>
2022-01-11 09:39:03 +0100

    de code deed dubbel werk, nu niet meer

--- Password_GeneratorFuncties/Password_Gen.py
+++ Password_GeneratorFuncties/Password_Gen.py
@@ -1,14 +1,11 @@
 import random
 import string
 
-string.ascii_letters
-string.digits
-string.punctuation
-
 randomletter = random.choice(string.ascii_letters)
 randomletterC = random.choice(string.ascii_letters).upper()
 randomdigit = random.choice(string.digits)
 randomspecial = random.choice(string.punctuation)
+
 print(randomletter)
 print(randomletterC)
 print(randomdigit)
